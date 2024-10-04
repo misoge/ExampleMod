9eb0aa731c52fd8a43292642af18343f8384bdc6
레콘 <rekorn@misoge.com>
2024-10-04 23:35:18 +0900

    Add project template

---
+++ ExampleMod/ExampleMod.cs
@@ -0,0 +1,34 @@
+﻿// Copyright 2024. Misoge. All Rights Reserved.
+// ####### Your copyright here #######
+
+#nullable enable
+
+using HarmonyLib;
+using ModContainer;
+
+namespace ExampleMod
+{
+    public class ExampleMod : Mod
+    {
+        public override int LoadOrder => -1;
+
+        public ExampleMod()
+        {
+            Metadata = Metadata with
+            {
+                ModeType = ModeType.None,
+                PackageId = "com.example.mod",
+                Name = "Example Mod",
+                Description = "An example mod for ModContainer.",
+                Author = "Your Name",
+                Version = "1.0.0",
+                Tags = new[] { "example", "mod" },
+                Dependencies = new string[] { },
+                OptionalDependencies = new string[] { }
+            };
+
+            var harmony = new Harmony(Metadata.PackageId);
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ ExampleMod/LogoPatcher.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2024. Misoge. All Rights Reserved.
+// ####### Your copyright here #######
+
+#nullable enable
+
+using EggRaiders.UI.Components;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ExampleMod
+{
+    [HarmonyPatch(typeof(UIFlicker), "Start")]
+    public class LogoPatcher
+    {
+        [HarmonyPrefix]
+        public static void Prefix(UIFlicker __instance)
+        {
+            __instance.AlphaRange = new Vector2(1f, 2f);
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(UIFlicker __instance)
+        {
+            Debug.Log("UIFlicker.Start() has been patched!");
+        }
+    }
+}
